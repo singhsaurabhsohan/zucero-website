7a6b2d137e63febb4b4bca96e363378a7806c318
SAURABH SOHAN SINGH <singhsaurabhsohan@gmail.com>
2026-09-04 16:05:22 +0530

    Add Next.js robots metadata

---
+++ storefront/app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from "next";
+import { absoluteUrl } from "@/lib/site";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/cart", "/checkout", "/account"],
+      },
+    ],
+    sitemap: absoluteUrl("/sitemap.xml"),
+  };
+}
